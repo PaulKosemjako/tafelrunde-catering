9d7f31165288ce7a97af6782d92c9b79718084fd
Paul Kosemjako <kosemjako.paul@gmail.com>
2025-10-23 20:29:37 +0200

    Fix basePath for GH-Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  output: "export", //Static HTML data
+  images: {unoptimized: true},
+  basePath: "/tafelrunde-catering",
+  assetPrefix: "/tafelrunde-catering/"
+  //reactStrictMode: true,
 };
 
 export default nextConfig;
